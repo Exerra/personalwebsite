5784362d2294726ffa51c79b768d4db270e9ffb1
Exerra <exerra@exerra.xyz>
2022-04-25 01:01:15 +0300

    favicon

--- app/root.jsx
+++ app/root.jsx
@@ -10,7 +10,14 @@ import {
 import styles from "./styles/app.css"
 
 export function links() {
-	return [{rel: "stylesheet", href: styles}]
+	return [
+		{rel: "stylesheet", href: styles},
+		{
+			rel: "icon",
+			href: "https://cdn.exerra.xyz/png/pfp.png",
+			type: "image/png",
+		},
+	]
 }
 
 export const meta = () => ({
