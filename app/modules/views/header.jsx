8b8b2749b64e3bed9a5b81693f2811d851589962
Exerra <exerra@exerra.xyz>
2022-10-24 15:22:15 +0300

    changed name

--- app/modules/views/header.jsx
+++ app/modules/views/header.jsx
@@ -22,9 +22,9 @@ export default function header() {
 	return (
 		<div className={"container mt-16 justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full pt-52 lg:pt-16"}>
 			<div className={"lg:ml-20 justify-center md:justify-start max-w-xl mt-0 md:my-36"}>
-				<h1 className={"font-bold text-7xl lg:text-9xl text-center md:text-left"}>Amelia</h1><br/>
+				<h1 className={"font-bold text-7xl lg:text-9xl text-center md:text-left"}>Exerra</h1><br/>
 
-				<h3 className={"font-bold text-2xl lg:text-3xl text-center md:text-left"}>Full-stack developer <Divider /> she/her</h3><br/>
+				<h3 className={"font-bold text-2xl lg:text-3xl text-center md:text-left"}>Full-stack developer <Divider /> Latvia</h3><br/>
 
 				<div className="bg-theme text-gray-400 font-bold text-center items-center md:text-left fill-blue-400 space-x-5">
 					<Button href={"https://twitter.com/Exerra"} icon={twitter(48)} label={"Link to twitter"} />
